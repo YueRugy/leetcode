2f3f8704488b5423ee93daf31d49cf19d86ba919
yue <yueweichao0927@gmail.com>
2020-04-13 17:04:18 +0800

    fix1

---
+++ 链表/_21_合并两个有序链表.go
@@ -0,0 +1,10 @@
+package 链表
+
+//link https://leetcode-cn.com/problems/merge-two-sorted-lists/
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	for t1, t2 := l1, l2; t1 != nil; t1, t2 = t1.Next, t2.Next {
+		if t1.Val>t2.Val {
+			
+		}
+	}
+}
